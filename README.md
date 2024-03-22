6851af7a3f7ed500ae2c337aef63479acb69d6a2
justlearningguy <emil_email@bk.ru>
2024-03-22 20:56:50 +0400

    almost rebuilded app

---
+++ README.md
@@ -0,0 +1,7 @@
+# ROCK PAPER SCISSORS
+**Hi!It's one of my study projects.**<br>
+***It's "Rock Paper Scissors" game created with HTML, CSS and JS.***<br>
+> This program also counts the amount of wins, draws and loses.
+<img src="https://github.com/justlearningguy/rockpaperscissors/assets/163748552/49df6ea5-4fcc-4ee6-8d3e-6092950dd0c8" width="400px">
+
+
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const scissors = document.getElementById("scissors");
 const draws = document.getElementById("draw-num");
 const wins = document.getElementById("win-num");
 const loses = document.getElementById("lose-num");
-const comp_move = document.getElementById("comp-move");
-const player_move = document.getElementById("player-move");
 const result = document.getElementById("result");
+const youMovePhoto = document.getElementById("youMovePhoto");
+const computerMovePhoto = document.getElementById("computerMovePhoto");
 
 let draws_var = 0;
 let wins_var = 0;
@@ -15,78 +15,72 @@ let comp_move_var;
 let player_move_var;
 
 rock.onclick = function() {
+    youMovePhoto.src = "images/rock.svg";
     player_move_var = 1;
     comp_move_var = Math.floor(Math.random() * 3) +1;
     if(comp_move_var==1) {
+        computerMovePhoto.src = "images/rock.svg";
         draws_var+=1;
         draws.textContent = draws_var;
-        comp_move.textContent = "Rock";
-        player_move.textContent = "Rock";
-        result.textContent = "Draw"
+        result.textContent = "Draw";
     }
     else if(comp_move_var==2) {
+        computerMovePhoto.src = "images/paper.svg";
         loses_var+=1;
         loses.textContent = loses_var;
-        comp_move.textContent = "Paper";
-        player_move.textContent = "Rock";
         result.textContent = "Lose"
     }
     else {
+        computerMovePhoto.src = "images/scissors.svg";
         wins_var+=1;
         wins.textContent = wins_var;
-        comp_move.textContent = "Scissors";
-        player_move.textContent = "Rock";
         result.textContent = "Win"
     }
 }
 paper.onclick = function() {
+    youMovePhoto.src = "images/paper.svg";
     player_move_var = 2;
     comp_move_var = Math.floor(Math.random() * 3) +1;
     if(comp_move_var==1) {
+        computerMovePhoto.src = "images/rock.svg";
         wins_var+=1;
         wins.textContent = wins_var;
-        comp_move.textContent = "Rock";
-        player_move.textContent = "Paper";
         result.textContent = "Win"
     }
     else if(comp_move_var==2) {
+        computerMovePhoto.src = "images/paper.svg";
         draws_var+=1;
         draws.textContent = draws_var;
-        comp_move.textContent = "Paper";
-        player_move.textContent = "Paper";
         result.textContent = "Draw"
     }
     else {
+        computerMovePhoto.src = "images/scissors.svg";
         loses_var+=1;
         loses.textContent = loses_var;
-        comp_move.textContent = "Scissors";
-        player_move.textContent = "Paper";
         result.textContent = "Lose"
     }
 }
 
 scissors.onclick = function() {
+    youMovePhoto.src = "images/scissors.svg";
     player_move_var = 3;
     comp_move_var = Math.floor(Math.random() * 3) +1;
     if(comp_move_var==1) {
+        computerMovePhoto.src = "images/rock.svg";
         loses_var+=1;
         loses.textContent = loses_var;
-        comp_move.textContent = "Rock";
-        player_move.textContent = "Scissors";
         result.textContent = "Lose"
     }
     else if(comp_move_var==2) {
+        computerMovePhoto.src = "images/paper.svg";
         wins_var+=1;
         wins.textContent = wins_var;
-        comp_move.textContent = "Paper";
-        player_move.textContent = "Scissors";
         result.textContent = "Win"
     }
     else {
+        computerMovePhoto.src = "images/scissors.svg";
         draws_var+=1;
         draws.textContent = draws_var;
-        comp_move.textContent = "Scissors";
-        player_move.textContent = "Scissors";
         result.textContent = "Draw"
     }
 }
